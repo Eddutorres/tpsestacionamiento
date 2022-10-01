0c771da817c555bbcb7abbe319cc379e14a162e2
Manuel Cabrera <ma.cabrerad@duocuc.cl>
2022-10-01 16:07:27 -0400

    segundo commit de clone

--- resources/views/footer.php
+++ resources/views/footer.php
@@ -1,6 +1,6 @@
 <!--FOOTER-->
 <footer class="footer">
         <div class="contacto">
-            <p><strong>@ TPSV. Terminal Pacifico Sur Valparaíso Chile<</strong></p>
+            <p><strong>@ TPSV. Terminal Pacifico Sur Valparaíso Chile</strong></p>
         </div>
     </footer>
